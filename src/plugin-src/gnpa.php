9759557d135802dea6594942d3883818d8108c1d
Hakira-Shymuy <hakira.shymuy@gmail.com>
2025-05-13 17:36:24 +0200

    feat: firs commit plugin files

---
+++ src/plugin-src/gnpa.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Etch
+ *
+ * @package           Etch
+ * @author            Digital Gravy
+ * @copyright         2024 Digital Gravy
+ * @gplv2
+ *
+ * @wordpress-plugin
+ * Plugin Name:       GNPA
+ * Plugin URI:        https://preparacaoeautossuficiencia.pt
+ * Description:       Tema para o site da GNPA.
+ * Version:           0.0.1
+ * Requires at least: 6.7
+ * Requires PHP:      8.4
+ * Author:            Hakira Shymuy
+ * Author URI:        https://github.com/Hakira-Shymuy
+ * Text Domain:       gnpa-td
+ * License:           GPL v3 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+declare(strict_types=1);
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Define plugin directories and urls.
+ */
+define( 'GNPA_PLUGIN_FILE', __FILE__ );
+define( 'GNPA_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'GNPA_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
