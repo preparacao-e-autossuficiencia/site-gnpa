075f1d94650f5073759d753b68c6040649de8795
Hakira-Shymuy <hakira.shymuy@gmail.com>
2025-05-15 19:34:57 +0200

    skip: wp-env local alternative

---
+++ src/env-src/wordpress/wp-config.php
@@ -0,0 +1,16 @@
+<?php
+define('DB_NAME', getenv('WORDPRESS_DB_NAME') ?: 'site');
+define('DB_USER', getenv('WORDPRESS_DB_USER') ?: 'site');
+define('DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD') ?: 'site');
+define('DB_HOST', getenv('WORDPRESS_DB_HOST') ?: 'database');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+
+$table_prefix = 'wp_';
+
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_DISPLAY', false);
+define('FS_METHOD', 'direct');
+
+require_once ABSPATH . 'wp-settings.php';
