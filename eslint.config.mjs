8b28f74189340969306112b39b86a0e5eb82924d
Hakira-Shymuy <hakira.shymuy@gmail.com>
2025-05-29 00:13:48 +0200

    chore: eslint and tsconfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,59 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import prettier from 'eslint-config-prettier';
 
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-]);
+export default [
+	js.configs.recommended,
+	...tseslint.configs.recommended,
+	pluginReact.configs.flat.recommended,
+	prettier,
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				wp: true
+			}
+		},
+		rules: {
+			'no-console': ['warn', { allow: ['warn', 'error'] }],
+			camelcase: [
+				'error',
+				{
+					properties: 'never',
+					ignoreDestructuring: false,
+					ignoreImports: false,
+					ignoreGlobals: true
+				}
+			],
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{
+					argsIgnorePattern: '^_',
+					varsIgnorePattern: '^_',
+					caughtErrorsIgnorePattern: '^_'
+				}
+			],
+			'@typescript-eslint/ban-ts-comment': [
+				'error',
+				{
+					'ts-expect-error': 'allow-with-description',
+					'ts-ignore': 'allow-with-description'
+				}
+			]
+		}
+	},
+	{
+		files: ['**/*.{js,mjs,cjs,ts,tsx,jsx}']
+	},
+	{
+		ignores: [
+			'**/node_modules',
+			'**/dist/**',
+			'**/build/**',
+			'coverage/',
+			'src/plugin-src/vendor/'
+		]
+	}
+]
