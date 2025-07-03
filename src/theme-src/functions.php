529f5c410d716f84fe357a3098ceaacbb5d65f8f
Hakira-Shymuy <hakira.shymuy@gmail.com>
2025-07-03 22:05:47 +0200

    fix: enqueue theme global styles on the editor as well

--- src/theme-src/functions.php
+++ src/theme-src/functions.php
@@ -6,14 +6,31 @@
  * @since 0.0.1
  */
 
+/**
+ * Define theme style path
+ */
+define( 'GNPA_THEME_STYLE_PATH', '/assets/styles/styles.css' );
+
+/**
+ * Enqueue theme styles
+ */
+function enqueue_gnpa_theme_styles() {
+	$style_uri  = get_template_directory_uri() . GNPA_THEME_STYLE_PATH;
+	$style_file = get_template_directory() . GNPA_THEME_STYLE_PATH;
+
+	wp_enqueue_style(
+		'gnpa-theme-styles',
+		$style_uri,
+		array(),
+		filemtime( $style_file )
+	);
+}
+
+add_action( 'wp_enqueue_scripts', 'enqueue_gnpa_theme_styles' );
 add_action(
-	'wp_enqueue_scripts',
+	'after_setup_theme',
 	function () {
-		wp_enqueue_style(
-			'gnpa-theme-styles',
-			get_template_directory_uri() . '/assets/styles/styles.css',
-			array(),
-			filemtime( get_template_directory() . '/assets/styles/styles.css' )
-		);
+		add_theme_support( 'editor-styles' );
+		add_editor_style( GNPA_THEME_STYLE_PATH );
 	}
 );
