a49f2c05c3a5dd4cee3935c9c7d07eac17be915f
Hakira-Shymuy <hakira.shymuy@gmail.com>
2025-07-04 01:09:58 +0200

    hide: fix the fkin todo

--- src/blocks-src/src/feature-section/render.php
+++ src/blocks-src/src/feature-section/render.php
@@ -25,7 +25,8 @@ $image         = $attributes['image'] ?? '';
 			<?php endif; ?>
 
 			<?php if ( $content ) : ?>
-				<div class="inner-demo">//todo: fazer o fix a esta div
+				<div class="inner-demo">
+					<!-- todo: fazer o fix a esta div -->
 					<?php echo wp_kses_post( $content ); ?>
 				</div>
 			<?php endif; ?>
