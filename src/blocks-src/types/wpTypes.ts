8c8166075a257257f080cecb783b4d3c9a459868
Hakira-Shymuy <hakira.shymuy@gmail.com>
2025-07-05 00:07:25 +0200

    hide: improve wp types

--- src/blocks-src/types/wpTypes.ts
+++ src/blocks-src/types/wpTypes.ts
@@ -2,15 +2,23 @@ type RenderedText = {
 	rendered: string;
 };
 
-type PostTypes = {
+type PostTypes<T_Acf = Record<string, unknown>> = {
 	id: number;
 	slug: string;
 	title: RenderedText;
-	excerpt: RenderedText;
+	excerpt?: RenderedText;
 	content: RenderedText;
 	date: string;
 	link: string;
-	acf?: Record<string, unknown>;
+	acf?: T_Acf;
 };
 
-export interface EventoPostTypes extends PostTypes {}
+interface EventoAcfFields {
+	evento_local?: string;
+	evento_data_inicio?: string;
+	evento_data_fim?: string;
+	evento_mapa?: string;
+	evento_registo_aberto?: boolean;
+}
+
+export interface EventoPostTypes extends PostTypes<EventoAcfFields> {}
