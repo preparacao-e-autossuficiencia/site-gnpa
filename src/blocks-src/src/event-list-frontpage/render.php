4c407c5c57860a6ef52cf6b68aa6ce956057bf76
Hakira-Shymuy <hakira.shymuy@gmail.com>
2025-07-31 04:15:20 +0200

    hide: card html structure

--- src/blocks-src/src/event-list-frontpage/render.php
+++ src/blocks-src/src/event-list-frontpage/render.php
@@ -12,6 +12,7 @@ $heading = $attributes['heading'] ?? '';
 $description = $attributes['description'] ?? '';
 $second_description = $attributes['second_description'] ?? '';
 $events_count = $attributes['events_count'] ?? 2;
+$grid_columns = 2 === $events_count ? 'events-grid--two' : 'events-grid--multi';
 
 $events_query = new WP_Query(
 	array(
@@ -44,28 +45,54 @@ $events_query = new WP_Query(
 			<p>Não existem eventos</p>
 		<?php endif; ?>
 
-		<?php
-		while ( $events_query->have_posts() ) :
-			$events_query->the_post();
-			?>
-			<div class="event-card">
-				<?php if ( get_the_title() ) : ?>
-					<h2><?php the_title(); ?></h2>
-				<?php endif; ?>
+		<div class="grid <?php echo esc_attr( $grid_columns ); ?>">
+			<?php
+			while ( $events_query->have_posts() ) :
+				$events_query->the_post();
+				?>
+				<div class="event-card">
+					<?php if ( has_post_thumbnail() ) : ?>
+						<figure class="event-card__media">
+							<?php
+							the_post_thumbnail(
+								'full',
+								array(
+									'alt' => get_the_title(),
+									'class' => 'event-card__image',
+									'loading' => 'lazy',
+									'decoding' => 'async',
+									'fetchpriority' => 'low',
+								)
+							);
+							?>
+						</figure>
+					<?php endif; ?>
 
-				<?php if ( get_the_excerpt() ) : ?>
-					<p><?php the_excerpt(); ?></p>
-				<?php endif; ?>
+					<div class="event-card__body">
+						<?php if ( get_the_title() ) : ?>
+							<h3><?php the_title(); ?></h3>
+						<?php endif; ?>
 
-				<?php if ( get_field( 'evento_data_inicio' ) ) : ?>
-					<p><?php the_field( 'evento_data_inicio' ); ?></p>
-				<?php endif; ?>
+						<?php if ( get_the_excerpt() ) : ?>
+							<p><?php the_excerpt(); ?></p>
+						<?php endif; ?>
 
-				<?php if ( get_field( 'evento_local' ) ) : ?>
-					<p><?php the_field( 'evento_local' ); ?></p>
-				<?php endif; ?>
-			</div>
-		<?php endwhile; ?>
+						<?php if ( get_field( 'evento_data_inicio' ) ) : ?>
+							<p><?php the_field( 'evento_data_inicio' ); ?></p>
+						<?php endif; ?>
+
+						<?php if ( get_field( 'evento_local' ) ) : ?>
+							<p><?php the_field( 'evento_local' ); ?></p>
+						<?php endif; ?>
+					</div>
+					<div class="event-card__footer">
+						<a href="<?php the_permalink(); ?>" class="button button--primary">
+							<?php esc_html_e( 'Saiba mais', 'gnpa' ); ?>
+						</a>
+					</div>
+				</div>
+			<?php endwhile; ?>
+		</div>
 		<?php wp_reset_postdata(); ?>
 	</div>
 
